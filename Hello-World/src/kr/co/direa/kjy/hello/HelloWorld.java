7af68ecfee1bd8ce88f5aafcfc3436bf3004b45d
KJY <KJY@DESKTOP-UHIED77>
2022-01-10 15:46:17 +0900

    print method overloading

--- Hello-World/src/kr/co/direa/kjy/hello/HelloWorld.java
+++ Hello-World/src/kr/co/direa/kjy/hello/HelloWorld.java
@@ -3,9 +3,14 @@ package kr.co.direa.kjy.hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		print();
+		print("Jinyoung");
 	}
 
 	private static void print() {
 		System.out.println("HELLO WORLD!!!");
+	}
+	
+	private static void print(String name) {
+		System.out.println("HELLO! " + name);
 	}
 }
